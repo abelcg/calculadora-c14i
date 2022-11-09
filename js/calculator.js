4941296c909a9f2b4c440293dc578feecac1ff97
abelcg <abelcg86@gmail.com>
2022-11-09 20:35:11 -0300

    calculadora terminada

--- js/calculator.js
+++ js/calculator.js
@@ -3,26 +3,58 @@ let buttons = document.querySelectorAll('button');
 
 //console.log(buttons);
 
-let screeValue = "";
+let screenValue = "";
+
+/* const handleChange = (event) => {
+    console.log(event.target.value)
+}
+
+screen.addEventListener('change', handleChange); */
 
 for(let item of buttons) {
     item.addEventListener('click', (e) => { 
-       // console.log(e.target.innerHTML);
         let buttonText = e.target.innerHTML;
         console.log('Button text is ' + buttonText);
 
         switch (buttonText) {
+            case 'X':
+                buttonText = "*";
+                console.log('Button text is ' + buttonText);
+                screenValue += buttonText;
+                screen.value = screenValue;
+                break;
             case 'AC':
-                screeValue = "";
-                screen.value = screeValue;
+                screenValue = "";
+                screen.value = screenValue;
+                break;
+            case '√':
+                buttonText = "sqrt(";
+                console.log('Button text is ' + buttonText);
+                screenValue += buttonText;
+                screen.value = screenValue;
+                break;
+            case 'π':
+                buttonText = "3.1416";
+                console.log('Button text is ' + buttonText);
+                screenValue += buttonText;
+                screen.value = screenValue;
+                break;
+            case '%':
+                buttonText = "/100*";
+                console.log('Button text is ' + buttonText);
+                screenValue += buttonText;
+                screen.value = screenValue;
                 break;
             case '=':
-                screeValue = "";
-                screen.value = eval(screeValue);
+                screen.value = math.evaluate(screenValue);
+                break;
+            case '⌫':
+                screenValue = screenValue.substring(0, screen.value.length - 1);
+                screen.value = screenValue;
                 break;
             default:
-                screeValue += buttonText;
-                screen.value = screeValue;
+                screenValue += buttonText;
+                screen.value = screenValue;
                 break;
         }
 
